498b8d6fedbb35b3b9e05cf3efbd17c1857f8606
Ali Shah <s.ashah@outlook.com>
2022-03-17 16:46:53 -0400

    [Renamed] game-board -> gameboard

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 import Grid from './js/Grid.js';
 
-const gameboard = document.querySelector('#game-board');
+const gameboard = document.querySelector('#gameboard');
 const grid = new Grid(gameboard);
+
+grid.randomEmptyCell().tile = new Tile(gameboard);
