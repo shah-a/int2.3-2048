4cfd48b5f2cbee11ea8523821ce88c28b62b6b48
Ali Shah <s.ashah@outlook.com>
2022-03-17 21:39:51 -0400

    [Update] bootstrapped input handlers

--- src/script.js
+++ src/script.js
@@ -11,3 +11,26 @@ const grid = new Grid(gameboard);
 
 grid.randomEmptyCell().tile = new Tile(gameboard);
 grid.randomEmptyCell().tile = new Tile(gameboard);
+
+function setupInput() {
+  window.addEventListener('keydown', handleInput, { once: true });
+}
+
+function handleInput(e) {
+  switch (e.key) {
+    case 'ArrowUp':
+      moveUp();
+      break;
+    case 'ArrowDown':
+      moveDown();
+      break;
+    case 'ArrowLeft':
+      moveLeft();
+      break;
+    case 'ArrowRight':
+      moveRight();
+      break;
+    default:
+      break;
+  }
+}
